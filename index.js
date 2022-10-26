b07fdf3ca41fb0b25727ffbdca61797203cf8fc0
wamarinj <walteramj@gmail.com>
2022-10-26 16:47:54 -0500

    corrigiendo cositas

--- index.js
+++ index.js
@@ -1,48 +1,65 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs/promises");
+const express = require("express");
+const app = express();
+app.use(express.json())
 
 
 const PORT = 8000;
 
-const app = http.createServer(async (request, response) => {
+const jsonPath = path.resolve("./data.json");
 
-    const method = request.method;
-    const url = request.url;
-    if (url === '/tasks') {
-        const jsonPath = path.resolve("./data.json");
+
+
+app.get('/tasks', async (req, res) => {
+    const jsonFile = await fs.readFile(jsonPath, "utf-8");
+    res.setHeader("Content-Type", "application/json");
+    res.write(jsonFile);
+    res.end()
+})
+
+app.post('/tasks', async (req, res) => {
+    try {
         const jsonFile = await fs.readFile(jsonPath, "utf-8");
-        if (method === 'GET') {
-            response.setHeader("Content-Type", "application/json");
-            response.write(jsonFile);
-        }
-        if (method === 'POST') {
-            console.log(request.body);
-            request.on("data", (data) => {
-                const newTask = JSON.parse(data);
-                const arr = JSON.parse(jsonFile);
-                arr.push(newTask);
-                console.log(arr);
-            });
-        }
-        if (method === 'PUT'){
-            request.on("data", (data) => {
-                const putTask = JSON.parse(data);
-                const putArr = JSON.parse(jsonFile);
-                putArr.push(putTask);
-            })
-        }
-        if (method === 'DELETE') {
-            request.on("data", (data) => {
-                const deleteTask = JSON.parse(data);
-                const delArr = JSON.parse(jsonFile);
-                delArr.push(deleteTask);
-                console.log(delArr);
-            })
-        }
-        }
-    response.end();
-});
+        req.on("data", async (data) => {
+            const newTask = JSON.parse(data);
+            const arr = JSON.parse(jsonFile);
+            arr.push(newTask);
+            await fs.writeFile(jsonPath, JSON.stringify(arr))
+            console.log(arr);
+        });
+        res.statusCode = 201
+    } catch (error) {
+        res.statusCode = 400
+        res.statusMessage = "Failure data"
+    }
+    res.end();
+
+})
+
+app.put("/tasks", async (req, res) => {
+    const jsonFile = await fs.readFile(jsonPath, "utf-8");
+    console.log(req.body)
+    const { id, status } = req.body;
+    const data = JSON.parse(jsonFile)
+    const taskId = data.findIndex((data) => data.id === id)
+    data[taskId].status = status
+    await fs.writeFile(jsonPath, JSON.stringify(data))
+    res.sendStatus(204);
+    res.end();
+})
+
+app.delete("/tasks", async (req, res) => {
+    const jsonFile = await fs.readFile(jsonPath, "utf-8");
+    const { id } = req.body;
+    const data = JSON.parse(jsonFile)
+    const taskId = data.findIndex((data) => data.id === id)
+    data.splice(taskId, 1)
+    await fs.writeFile(jsonPath, JSON.stringify(data))
+    res.sendStatus(204);
+    res.end();
+})
 
 app.listen(PORT);
 
